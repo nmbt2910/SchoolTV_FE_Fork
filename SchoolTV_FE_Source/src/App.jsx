ff5123c88cde5ba3cf09ee976cf078a27e3306a8
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2025-01-18 11:46:39 +0700

    test2

--- SchoolTV_FE_Source/src/App.jsx
+++ SchoolTV_FE_Source/src/App.jsx
@@ -4,6 +4,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home';
+import meow from './pages/meow';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -17,6 +18,16 @@ function App() {
         <>
           <Header /> {/* Include the Header component */}
           <Home />   {/* Include the Home component */}
+          <Footer /> {/* Include the Footer component alo alo 1234 test etstte */}
+        </>
+      ),
+    },
+    {
+      path: "/meow",
+      element: (
+        <>
+          <Header /> {/* Include the Header component */}
+          <meow />   {/* Include the Home component */}
           <Footer /> {/* Include the Footer component alo alo 1234 test etstte */}
         </>
       ),
