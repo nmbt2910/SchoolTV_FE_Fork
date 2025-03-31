7b7f9f048d007d4cae0c9728496362409a3cbde5
haoltse182646 <haoltse182646@fpt.edu.vn>
2025-03-31 14:06:07 +0700

    Update menu list with API config

--- SchoolTV_FE_Source/src/pages/AdminPage/UserList.jsx
+++ SchoolTV_FE_Source/src/pages/AdminPage/UserList.jsx
@@ -5,7 +5,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import apiFetch from '../../config/baseAPI';
 
-const { SubMenu } = Menu;
 const { Sider, Content } = Layout;
 const { Search } = Input;
 const { Option } = Select;
@@ -212,16 +211,46 @@ function UserList() {
     <div className="userlist-body">
       <Layout style={{ minHeight: '90vh' }}>
         <Sider width={225} className="site-layout-background">
-          <Menu theme='dark' mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
-            <Menu.Item key="1" icon={<UnorderedListOutlined />}><Link to="/adminpage">Dashboard</Link></Menu.Item>
-            <Menu.Item key="2" icon={<SettingOutlined />}><Link to="/sopending">School Owner Pending</Link></Menu.Item>
-            <SubMenu key="3" icon={<UserOutlined />} title="User Management">
-              <Menu.Item key="3.1" icon={<UserDeleteOutlined />}><Link to="/userlist">User List</Link></Menu.Item>
-              <Menu.Item key="3.2" icon={<UsergroupDeleteOutlined />}><Link to="/adminlist">Admin List</Link></Menu.Item>
-              <Menu.Item key="3.3" icon={<HomeOutlined />}><Link to="/soaccount">School Owner Account</Link></Menu.Item>
-            </SubMenu>
-            <Menu.Item key="4" icon={<LogoutOutlined />} onClick={handleLogout}>Log out</Menu.Item>
-          </Menu>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={[
+            {
+              key: '1',
+              icon: <UnorderedListOutlined />,
+              label: <Link to="/adminpage">Dashboard</Link>,
+            },
+            {
+              key: '2',
+              icon: <SettingOutlined />,
+              label: <Link to="/sopending">School Owner Pending</Link>,
+            },
+            {
+              key: '3',
+              icon: <UserOutlined />,
+              label: "User Management",
+              children: [
+                {
+                  key: '3.1',
+                  icon: <UserDeleteOutlined />,
+                  label: <Link to="/userlist">User List</Link>,
+                },
+                {
+                  key: '3.2',
+                  icon: <UsergroupDeleteOutlined />,
+                  label: <Link to="/adminlist">Admin List</Link>,
+                },
+                {
+                  key: '3.3',
+                  icon: <HomeOutlined />,
+                  label: <Link to="/soaccount">School Owner Account</Link>,
+                },
+              ],
+            },
+            {
+              key: '4',
+              icon: <LogoutOutlined />,
+              label: 'Log out',
+              onClick: handleLogout,
+            },
+          ]} />
         </Sider>
 
         <Layout style={{ padding: '0 24px 24px' }}>
